270d41522d256e49a572cd90f45223d8e9b7281b
odjhey <odjhey@gmail.com>
2026-02-06 08:40:39 +0800

    update vite config to support remote work

--- webapp-client/vite.config.ts
+++ webapp-client/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+	// Load environment variables for the current mode
+	// The third argument (optional) is the prefix; use '' to load all env variables
+
+	const env = loadEnv(mode, process.cwd(), "");
+	return {
+		server: {
+			host: "0.0.0.0",
+			allowedHosts: env.ALLOWED_HOSTS
+				? env.ALLOWED_HOSTS.split(",")
+				: ["localhost"],
+		},
+		plugins: [react()],
+	};
+});
